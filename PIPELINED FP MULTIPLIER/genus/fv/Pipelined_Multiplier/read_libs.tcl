90b9bb62b2330e43d4b10f76896ef06eb4e3d32a
maazahmed23456 <116651816+maazahmed23456@users.noreply.github.com>
2025-03-26 19:39:59 +0530

    Delete PIPELINED FP MULTIPLIER directory

--- PIPELINED FP MULTIPLIER/genus/fv/Pipelined_Multiplier/read_libs.tcl
+++
@@ -1,5 +0,0 @@
-add_search_path /home/ece_student/ec499_maaz/Pipelined_Multiplier/genus/library -library -both
-read_library -liberty -both \
-    /home/ece_student/ec499_maaz/Pipelined_Multiplier/genus/library/fast.lib \
-    /home/ece_student/ec499_maaz/Pipelined_Multiplier/genus/library/fast.lib
-
--- PIPELINED FP MULTIPLIER/genus/genus.cmd
+++
@@ -1,9 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 21.14-s082_1, built Jun 23 2022 14:32:08
-
-# Date: Wed Mar 05 15:03:44 2025
-# Host: vlsilab4.nitandhra.ac.in (x86_64 w/Linux 4.18.0-425.3.1.el8.x86_64) (12cores*20cpus*1physical cpu*12th Gen Intel(R) Core(TM) i7-12700 25600KB)
-# OS:   Red Hat Enterprise Linux release 8.7 (Ootpa)
-
-source synthesis.tcl
-gui_hide
-exit
